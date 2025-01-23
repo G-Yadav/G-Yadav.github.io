087dd4696f5c2dca724a7e1200601bef5552f471
Macklov <yadavgaurav2107@gmail.com>
2025-01-23 11:30:21 +0530

    Upgrade Angular dependencies to version 19 and update component metadata for standalone support

--- src/app/header/header-socials/header-socials.component.ts
+++ src/app/header/header-socials/header-socials.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header-socials',
-  templateUrl: './header-socials.component.html',
-  styleUrls: ['./header-socials.component.scss']
+    selector: 'app-header-socials',
+    templateUrl: './header-socials.component.html',
+    styleUrls: ['./header-socials.component.scss'],
+    standalone: false
 })
 export class HeaderSocialsComponent implements OnInit {
 
